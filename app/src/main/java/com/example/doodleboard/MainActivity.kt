4bd01e6b73f5884c990d341ee67fd107d0e8a998
isHiteshBehera <bhiteshme74@gmail.com>
2023-07-27 19:14:19 +0530

    fixed Ink Change

--- app/src/main/java/com/example/doodleboard/MainActivity.kt
+++ app/src/main/java/com/example/doodleboard/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.doodleboard
 
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.Toast
+import com.example.doodleboard.DoodleView.Companion.currentInk
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,19 +27,33 @@ class MainActivity : AppCompatActivity() {
         var eraserBtn = findViewById<ImageButton>(R.id.whiteColor);
 
         redBtn.setOnClickListener {
-            Toast.makeText(this, "Clicked Red", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Clicked Red", Toast.LENGTH_SHORT).show();
+            stylusNib.color = Color.RED;
+            currentColor(stylusNib.color);
         }
 
         blueBtn.setOnClickListener {
             Toast.makeText(this, "Clicked Blue", Toast.LENGTH_SHORT).show()
+            stylusNib.color = Color.BLUE;
+            currentColor(stylusNib.color);
         }
 
         blackBtn.setOnClickListener {
             Toast.makeText(this, "Clicked Black", Toast.LENGTH_SHORT).show()
+            stylusNib.color = Color.BLACK;
+            currentColor(stylusNib.color);
         }
 
         eraserBtn.setOnClickListener {
             Toast.makeText(this, "Clicked Eraser", Toast.LENGTH_SHORT).show()
+            stylusNib.color = Color.WHITE;
+            currentColor(stylusNib.color);
         }
     }
+
+    private fun currentColor(color: Int) {
+        currentInk = color;
+        path = Path();
+    }
+
 }
